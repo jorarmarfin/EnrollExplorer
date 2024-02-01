fad19e38172121199de1265907c61dcade0acfbe
Luis Mayta <luis.mayta@gmail.com>
2024-02-01 14:57:37 -0500

    Footer

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -273,13 +273,13 @@
                     <!--begin::Copyright-->
                     <div class="text-dark order-2 order-md-1">
                         <span class="text-gray-400 fw-semibold me-1">Created by</span>
-                        <a href="https://keenthemes.com" target="_blank" class="text-muted text-hover-primary fw-semibold me-2 fs-6">DIAD</a>
+                        <a href="https://keenthemes.com" target="_blank" class="text-muted text-hover-primary fw-semibold me-2 fs-6">luis.mayta</a>
                     </div>
                     <!--end::Copyright-->
                     <!--begin::Menu-->
                     <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
                         <li class="menu-item">
-                            <a href="http://admision.uni.edu.pe/" target="_blank" class="menu-link px-2">Dirección de admisión</a>
+                            <a href="http://admision.uni.edu.pe/" target="_blank" class="menu-link px-2">CAL</a>
                         </li>
                     </ul>
                     <!--end::Menu-->
