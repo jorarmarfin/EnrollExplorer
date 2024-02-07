65783eeec96fca84f27cb2952de87f4d5bd3ef7c
Luis Mayta <luis.mayta@gmail.com>
2024-02-06 23:18:51 -0500

    ruta de log

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('index');
 })->middleware(['auth', 'verified'])->name('home');
@@ -50,3 +39,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
